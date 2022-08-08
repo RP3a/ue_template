c52ac9ec93cf24fa63bb6086b5ed15f78c9c3c39
RP3a <petria.robert@gmail.com>
2022-08-08 16:51:40 +0300

    ue_template

---
+++ src/ue_template_assets/src/index.js
@@ -0,0 +1,19 @@
+import { ue_template } from "../../declarations/ue_template";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await ue_template.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
